ef280364ebe5fbe10e99e3a60b7e0c938b09fb30
Vishal Prajapati <127819340+vishal2231179@users.noreply.github.com>
2026-08-13 03:33:47 +0530

    commit

--- scripts/hamburger.js
+++
@@ -1,9 +0,0 @@
-/* mobile nav */
-const navToggle = document.getElementById('navToggle');
-const navList = document.getElementById('navList');
-
-navToggle.addEventListener('click', () => navList.classList.toggle('open'));
-
-navList.querySelectorAll('a').forEach(a =>
-  a.addEventListener('click', () => navList.classList.remove('open'))
-);
